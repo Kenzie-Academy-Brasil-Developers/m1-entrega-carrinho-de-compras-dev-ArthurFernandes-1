128e54d96d6411ceee42956a1ada6fc4d78315ce
Arthur Fernandes <arthurdesousafernandes@gmail.com>
2022-09-06 11:57:53 -0300

    MercadinhoDaEsquina phase1

---
+++ produtos.js
@@ -0,0 +1,57 @@
+const productsCart = [
+    {
+        id: 1,
+        name: "Chinelo Nuvem",
+        img: {"todos":"/img/produtos/chineloNuvem/chinelos.jfif","branco":"/img/produtos/chineloNuvem/chinelo-BRANCO.png","preto": "/img/produtos/chineloNuvem/chinelo-PRETO.png"},
+        price: 75.00
+    },
+    {
+        id: 2,
+        name: "Abridor de latas",
+        img: {"demonstracao1": "/img/produtos/abridorLata/demonstracao.png", "demonstracao2":"/img/produtos/abridorLata/demonstracao2.png","preto":"/img/produtos/abridorLata/abridor-PRETO.png","cinza":"/img/produtos/abridorLata/abridor-CINZA.png"},
+        price: 25.00
+    },
+    {
+        id: 3,
+        name: "Nike Air Jordan",
+        img: {"preto":["/img/produtos/sapatos/AirJordan-Preto-FRENTE.png","/img/produtos/sapatos/AirJordan-Preto-DIREITA"],"vermelho":["/img/produtos/sapatos/AirJordan-Vermelho-FRENTE.png","/img/produtos/sapatos/AirJordan-Vermelho-DIREITA.png"]},
+        price: 1800.00
+    },
+    {
+        id: 4,
+        name: "Nike Air Max 90",
+    img: {"rosa":["/img/produtos/sapatos/AirMax-Rosa-FRENTE.png","/img/produtos/sapatos/AirMax-Rosa-ESQUERDO.png"],"preto":["/img/produtos/sapatos/AirMax-Preto-FRENTE.png","/img/produtos/sapatos/AirMax-Preto-ESQUERDO.png"]},
+        price: 729.00
+    },
+    {
+        id: 5,
+        name: "Kit de Panela de pedra",
+        img: {"conjunto": "/img/produtos/panelas/conjunto-PanelaPedra.png","panela":["/img/produto/panelas/panela-PanelaPedra.png"]},
+        price: 0.00
+    },
+    {
+        id: 1,
+        name: "",
+        price: 0.00
+    },
+    {
+        id: 1,
+        name: "",
+        price: 0.00
+    },
+    {
+        id: 1,
+        name: "",
+        price: 0.00
+    },
+    {
+        id: 1,
+        name: "",
+        price: 0.00
+    },
+    {
+        id: 1,
+        name: "",
+        price: 0.00
+    },
+]
